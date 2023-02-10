a9b7eacf3bb88d74dbde8da13266e4193e520d68
Timothy <sanohoang@gmail.com>
2023-02-09 18:59:12 -0600

    .

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.removeAttribute('id');
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = "JavaScript is the champion";
+
+document.body.append(newHeader);
